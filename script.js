39356032f262bd762c31f8085dff97dbadc03de8
Freddy <freddychura93@gmail.com>
2025-04-29 08:22:26 -0400

    Primer commit ejercicio Guiado JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+// Obtener elementos del DOM
+const verDetallesButton = document.getElementById('verDetalles');
+const volverButton = document.getElementById('volver');
+const tarjeta = document.getElementById('tarjeta');
+
+// Función para voltear la tarjeta
+verDetallesButton.addEventListener('click', () => {
+    tarjeta.classList.add('volteada');
+});
+
+// Función para volver a la cara frontal
+volverButton.addEventListener('click', () => {
+    tarjeta.classList.remove('volteada');
+});
